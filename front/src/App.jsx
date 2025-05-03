662d6b5d38410dcc5d071f968dfa9ad910bd35f1
unknown <khalidbouychou@users.noreply.github.com>
2025-05-03 12:05:24 +0100

    fix tailwind css

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import Questions from "./pages/questions";
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-yellow-300 text-white h-screen flex justify-center items-center">
+     <Questions />
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ front/src/pages/questions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const questions = () => {
+    return (
+        <div className='bg-blue-700 flex justify-center items-center'>
+            <h1>this questions section</h1>
+        </div>
+    )
+}
+
+export default questions
